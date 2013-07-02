ef59ca57eca8fbca6403471133019a4898146117
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-02 10:34:43 -0500

    bakery item model updated

--- cookie_model.js
+++ cookie_model.js
@@ -1,3 +1,5 @@
+// Pseudocode
+
 // Cookie model
 // knows about its state, kind, "done time"
 
@@ -9,34 +11,6 @@
 // has to be able to bake things (mess with the state of the cookies)
 // identify state of cookies
 
-
-var Cookie = function(name, baking_time){
-  this.name = name;
-  var baking_time = baking_time;
-  var timeInOven = 0;
-};
-
-Cookie.prototype = {
-         bake : function(){
-          this.timeInOven ++1;
-         },
-         current_status : function()
-}
-
-Cookie.prototype.current_status = function(){
-    if (timeInOven === 0) {
-      return 'Raw'
-    } else if (this.timeInOven < baking_time){
-      return 'still_gooey'
-    } else if (this.timeInOven === baking_time){
-      return 'just_right'
-    } else {
-      return 'crispy'
-    };
-
-}
-
-
 // current_status --> e.g. raw
 // baking_time --> 4 minutes
 // time on oven --> 2 minutes
@@ -46,3 +20,36 @@ Cookie.prototype.current_status = function(){
 // Oven --> Cookie : whats ur status?
 // Cookie.current_status
  // if timeInOven = cookie.baking_time/4 --> xxx
+
+
+// Bakery Item Model
+var BakeryItem = function (type, name, time) {
+    this.attributes = {
+      type: type,
+      name: name,
+      bakingTime: time,
+      timeInOven: 0
+    };
+};
+
+
+BakeryItem.prototype = {
+    // Left this in for later reference
+    // getName: function() {
+    //   return this.attributes.name;
+    // },
+    bake: function () {
+        this.attributes.timeInOven++;
+    },
+    current_status: function () {
+        if (this.attributes.timeInOven === 0) {
+            return 'Raw';
+        } else if (this.attributes.timeInOven < this.attributes.bakingTime) {
+            return 'still_gooey';
+        } else if (this.attributes.timeInOven === this.attributes.bakingTime) {
+            return 'just_right';
+        } else {
+            return 'crispy';
+        }
+    }
+};
